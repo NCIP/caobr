82d8722cd686760ac202feca096eb3154f748da3
chandrakant <chandrakant>
2009-12-04 13:06:13 +0000

    Using the service
    SVN-Revision: 10

--- software/java-swing-client/src/edu/wustl/caobr/client/ServiceInvoker.java
+++ software/java-swing-client/src/edu/wustl/caobr/client/ServiceInvoker.java
@@ -4,7 +4,8 @@ import java.rmi.RemoteException;
 import java.util.Arrays;
 import java.util.Comparator;
 
-import org.cagrid.caobr.service.CaObrImpl;
+import org.apache.axis.types.URI.MalformedURIException;
+import org.cagrid.caobr.client.CaObrClient;
 
 import edu.wustl.caobr.Annotation;
 import edu.wustl.caobr.Ontology;
@@ -16,90 +17,30 @@ import edu.wustl.caobr.Resource;
  */
 public class ServiceInvoker {
     
-//    private static final String SERVICE_URL = "http://caobr.wustl.edu:8080/wsrf/services/cagrid/CaObr";
-//    //private static final String SERVICE_URL = "http://ps4902:8080/wsrf/services/cagrid/CaObr";
-//
-//    private CaObrClient client = null;
-//
-//    public ServiceInvoker() {
-//
-//        try {
-//            client = new CaObrClient(SERVICE_URL);
-//        } catch (MalformedURIException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        } catch (RemoteException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//
-//    }
-//
-//    public Ontology[] getOntologies() {
-//
-//        Ontology[] ontologies = null;
-//        try {
-//            ontologies = client.getAllOntologies();
-//        } catch (RemoteException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//        Arrays.sort(ontologies, new Comparator<Ontology>() {
-//            public int compare(Ontology o1, Ontology o2) {
-//                return o1.getDisplayLable().compareToIgnoreCase(o2.getDisplayLable());
-//            }
-//        });
-//        System.out.println("------------------------\n\tOntologies\n------------------------");
-//        for (Ontology o : ontologies) {
-//            System.out.println(o.getDisplayLable());
-//        }
-//        return ontologies;
-//    }
-//
-//    public Resource[] getResources() {
-//
-//        Resource[] resources = null;
-//        try {
-//            resources = client.getAllResources();
-//        } catch (RemoteException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//        Arrays.sort(resources, new Comparator<Resource>() {
-//            public int compare(Resource r1, Resource r2) {
-//                return r1.getName().compareToIgnoreCase(r2.getName());
-//            }
-//        });
-//        System.out.println("------------------------\n\tResources\n------------------------");
-//        for (Resource r : resources) {
-//            System.out.println(r.getName());
-//        }
-//        return resources;
-//    }
-//
-//    public Annotation[] getAnnotations(Ontology[] ontologies, Resource[] resources, String token) {
-//        Annotation[] annotations = new Annotation[0];
-//        try {
-//            annotations = client.getAnnotations(ontologies, resources, token);
-//        } catch (RemoteException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//        }
-//        return annotations;
-//    }
-    
-///////////////////////////////////////////////////////////////////////////
-    
-    static {
-        System.setProperty("http.proxyHost","ptproxy.persistent.co.in");
-        System.setProperty("http.proxyPort","8080");
-                           
+    private static final String SERVICE_URL = "http://caobr.wustl.edu:8080/wsrf/services/cagrid/CaObr";
+    //private static final String SERVICE_URL = "http://ps4902:8080/wsrf/services/cagrid/CaObr";
+
+    private CaObrClient client = null;
+
+    public ServiceInvoker() {
+
+        try {
+            client = new CaObrClient(SERVICE_URL);
+        } catch (MalformedURIException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
     }
+
     public Ontology[] getOntologies() {
 
         Ontology[] ontologies = null;
         try {
-            ontologies = new CaObrImpl().getAllOntologies();
+            ontologies = client.getAllOntologies();
         } catch (RemoteException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -120,7 +61,7 @@ public class ServiceInvoker {
 
         Resource[] resources = null;
         try {
-            resources = new CaObrImpl().getAllResources();
+            resources = client.getAllResources();
         } catch (RemoteException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -140,13 +81,73 @@ public class ServiceInvoker {
     public Annotation[] getAnnotations(Ontology[] ontologies, Resource[] resources, String token) {
         Annotation[] annotations = new Annotation[0];
         try {
-            annotations = new CaObrImpl().getAnnotations(ontologies, resources, token);
+            annotations = client.getAnnotations(ontologies, resources, token);
         } catch (RemoteException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return annotations;
     }
+    
+///////////////////////////////////////////////////////////////////////////
+    
+//    static {
+//        System.setProperty("http.proxyHost","ptproxy.persistent.co.in");
+//        System.setProperty("http.proxyPort","8080");
+//                           
+//    }
+//    public Ontology[] getOntologies() {
+//
+//        Ontology[] ontologies = null;
+//        try {
+//            ontologies = new CaObrImpl().getAllOntologies();
+//        } catch (RemoteException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        Arrays.sort(ontologies, new Comparator<Ontology>() {
+//            public int compare(Ontology o1, Ontology o2) {
+//                return o1.getDisplayLable().compareToIgnoreCase(o2.getDisplayLable());
+//            }
+//        });
+//        System.out.println("------------------------\n\tOntologies\n------------------------");
+//        for (Ontology o : ontologies) {
+//            System.out.println(o.getDisplayLable());
+//        }
+//        return ontologies;
+//    }
+//
+//    public Resource[] getResources() {
+//
+//        Resource[] resources = null;
+//        try {
+//            resources = new CaObrImpl().getAllResources();
+//        } catch (RemoteException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        Arrays.sort(resources, new Comparator<Resource>() {
+//            public int compare(Resource r1, Resource r2) {
+//                return r1.getName().compareToIgnoreCase(r2.getName());
+//            }
+//        });
+//        System.out.println("------------------------\n\tResources\n------------------------");
+//        for (Resource r : resources) {
+//            System.out.println(r.getName());
+//        }
+//        return resources;
+//    }
+//
+//    public Annotation[] getAnnotations(Ontology[] ontologies, Resource[] resources, String token) {
+//        Annotation[] annotations = new Annotation[0];
+//        try {
+//            annotations = new CaObrImpl().getAnnotations(ontologies, resources, token);
+//        } catch (RemoteException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        return annotations;
+//    }
     
     
 }
